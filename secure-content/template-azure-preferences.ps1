dfe3c9d97face247a9dcafa0ccbc83770e363d8b
Ong Zhixian <zhixian@hotmail.com>
2018-11-22 11:32:02 +0800

    add template for azure preferences

---
+++ secure-content/template-azure-preferences.ps1
@@ -0,0 +1,5 @@
+################################################################################
+# This is a template file that is utilitied by other scripts that references user preferences for certain settings in Azure
+# $location = A string representing preferred location
+
+$location   = "xxxxxxxxxxxxxx"
