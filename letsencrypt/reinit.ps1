ef1f007c820a484499f082b532036f145c686836
zhixian <zhixian@hotmail.com>
2019-10-31 21:28:36 +0800

    Update scripts and notes for site specific instructions

--- letsencrypt/reinit.ps1
+++ letsencrypt/reinit.ps1
@@ -28,3 +28,4 @@ New-ACMEIdentifier -Dns plato-trading-bot.plato.emptool.com -Alias plato_trading
 New-ACMEIdentifier -Dns brahman-devops.plato.emptool.com    -Alias brahman_devops_plato_dns
 New-ACMEIdentifier -Dns plato-dev-bot.plato.emptool.com     -Alias plato_dev_bot_plato_dns
 New-ACMEIdentifier -Dns wordpress.plato.emptool.com         -Alias wordpress_plato_dns
+New-ACMEIdentifier -Dns csi.plato.emptool.com               -Alias csi_plato_dns
